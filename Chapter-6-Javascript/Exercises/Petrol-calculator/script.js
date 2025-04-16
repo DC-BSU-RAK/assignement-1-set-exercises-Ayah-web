2bca1657cae99f1f5f6c016ce9633f89b28662a7
Ayahtullah Centeno <Ayahfahmy2007@gmail.com>
2025-04-16 19:17:22 +0400

    added references to all elements

---
+++ Chapter-6-Javascript/Exercises/Petrol-calculator/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // storage of references of elements in const
+    const costPerLiterInput = document.getElementById('cost-per-liter');
+    const litersInput = document.getElementById('liters');
+    const calculateButton = document.getElementById('calculate-btn');
+    const totalCostDisplay = document.getElementById('total-cost');
+
+
+});
