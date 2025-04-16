3f8ca1f91a0911043a4fdd3cb28d98812ace454a
Ayahtullah Centeno <Ayahfahmy2007@gmail.com>
2025-04-16 19:19:54 +0400

    added add click event listener for calculate button

--- Chapter-6-Javascript/Exercises/Petrol-calculator/script.js
+++ Chapter-6-Javascript/Exercises/Petrol-calculator/script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+
     // storage of references of elements in const
     const costPerLiterInput = document.getElementById('cost-per-liter');
     const litersInput = document.getElementById('liters');
     const calculateButton = document.getElementById('calculate-btn');
     const totalCostDisplay = document.getElementById('total-cost');
 
+    // adding click event listener for calculate button
+    calculateButton.addEventListener('click', calculateTotalCost);
 
 });
